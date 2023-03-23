22f007525c6ae8c02cee4b32729a4540874eab4a
Breno Mendonca <breno_a99@hotmail.com>
2023-03-23 01:16:56 -0300

    Adicionando a sessão das reuniões

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ sr.reveal('.home-text', {duration:2000})
 sr.reveal('.quem-somos-text', {duration:3000})
 
 sr.reveal('.Big-Numbers', {duration:3000})
+
+sr.reveal('.Kanban', {duration:3000})
+
+sr.reveal('.PlanningPoker', {duration:3000})
+
+sr.reveal('.Reuniao', {duration:3000})
